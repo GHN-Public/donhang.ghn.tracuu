cc288e796fba3921fa5b7d05aea7c317f85575ef
GHN-Public <ttthientan102@gmail.com>
2026-08-27 20:28:26 +0700

    Update telegram_sender.py

--- telegram_sender.py
+++ telegram_sender.py
@@ -1,18 +1,12 @@
 import requests
 from config import BOT_TOKEN, CHAT_ID
 
-BASE_URL = f"https://api.telegram.org/bot{BOT_TOKEN}"
-
 def send_message(text):
-    url = f"{BASE_URL}/sendMessage"
-
-    response = requests.post(
-        url,
-        data={
-            "chat_id": CHAT_ID,
-            "text": text
-        }
-    )
-
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": text
+    }
+    response = requests.post(url, data=payload)
     print(response.status_code)
     print(response.text)
